39a2f6a053649146a3a8aa2b1984751ae47d3bde
Joe Gelay <joegelay@gmail.com>
2020-01-14 21:05:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,10 +10,6 @@ def directors_totals(nds)
 
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
-  #
   #
   # Be sure to return the result at the end!
   
